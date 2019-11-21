30a9d25c998a2c4e84a8143bc57ba3967181972c
tahara-pg <tahara.pg@gmail.com>
2019-11-21 10:17:04 +0900

    本日の時刻経過度合を考慮

--- schedule.rb
+++ schedule.rb
@@ -4,6 +4,9 @@ require 'holiday_jp'
 class Schedule
   attr_reader :started_on, :due_on, :today
 
+  WORK_START_TIME = 9
+  WORK_END_TIME = 18
+
   def initialize(due_on)
     @started_on = Date.parse(Config.instance.started_on)
     @due_on = due_on
@@ -23,18 +26,30 @@ class Schedule
   # 残日数
   #
   def remaining_days
-    @remaining_dates.count
+    @remaining_dates.count - time_elapsed_rate_today
   end
 
   # 作業済日数
   #
   def worked_days
-    @worked_dates.count
+    @worked_dates.count + time_elapsed_rate_today
   end
 
   private
 
   def bussiness_dates(from, to)
     (from..to).select { |date| !HolidayJp.holiday?(date) && !date.saturday? && !date.sunday? }
+  end
+
+  # 本日の時刻経過度合
+  #
+  def time_elapsed_rate_today
+    if Time.now.hour <= WORK_START_TIME
+      1
+    elsif Time.now.hour >= WORK_END_TIME
+      0
+    else
+      (Time.now.hour - WORK_START_TIME).fdiv(WORK_END_TIME - WORK_START_TIME).round(1)
+    end
   end
 end
